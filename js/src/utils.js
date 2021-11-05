1a361d76d394296d8023770a14d135f5cf43887c
“zhaosuchen” <2759289793@qq.com>
2021-11-05 11:37:29 +0800

    Site updated: 2021-11-05 11:37:29

--- js/src/utils.js
+++ js/src/utils.js
@@ -320,12 +320,10 @@ $(document).ready(function () {
 
     // Initialize Sidebar & TOC Width.
     var scrollbarWidth = NexT.utils.getScrollbarWidth();
-      if ($('.site-overview-wrap').height() > (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
+      if ($('.sidebar-panel').height() > (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
         $('.site-overview').css('width', 'calc(100% + ' + scrollbarWidth + 'px)');
       }
-      if ($('.post-toc-wrap').height() > (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
-        $('.post-toc').css('width', 'calc(100% + ' + scrollbarWidth + 'px)');
-      }
+    $('.post-toc').css('width', 'calc(100% + ' + scrollbarWidth + 'px)');
 
     // Initialize Sidebar & TOC Height.
     updateSidebarHeight(document.body.clientHeight - NexT.utils.getSidebarSchemePadding());
